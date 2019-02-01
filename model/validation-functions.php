8589b8bf43ea2eefb33f80852de9891e984c5159
Amanda Williams <amandacwilliams97@gmail.com>
2019-02-01 18:51:31 -0500

    added validation doc

---
+++ model/validation-functions.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mandy
+ * Date: 2/1/2019
+ * Time: 3:48 PM
+ */
+/*
+ * Amanda Williams
+ * February 1, 2019
+ */
+
+//if form submit validate inputs
+
+function validName()
+{
+
+}
+
+function validAge()
+{
+
+}
+
+function validPhone()
+{
+
+}
+
+function validIndoor()
+{
+
+}
+
+function validOutdoor()
+{
+
+}
