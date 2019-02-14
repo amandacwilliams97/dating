a0168dfe188fe0c51296368718ddb31ae5f4c6d5
Amanda Williams <amandacwilliams97@gmail.com>
2019-02-13 19:56:36 -0500

    reorganized and fixed index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 /*
  * Amanda Williams
- * January 17, 2019
- * 328/dating/index.php
+ * February 13, 2019
+ * 328/dating/index1.php
  */
 
 #Start session
@@ -12,23 +12,22 @@ session_start();
 ini_set("display_errors", 1);
 error_reporting(E_ALL);
 
+#-------------------------------------------------------------------------------
 #require autoload
 require_once ('vendor/autoload.php');
 #require validation file
 require ('model/validation-functions.php');
 
+#-------------------------------------------------------------------------------
 #create an instance of the Base class
 $f3 = Base::instance();
 
 #Turn on Fat-Free error reporting
 $f3->set('DEBUG', 3);
 
-
 #-------------------------------------------------------------------------------
 #define a default route
-$f3->route('GET /', function($f3) {
-    //$f3->set('page',"home");
-    //$f3->set('title',"Home Page");
+$f3->route('GET /', function() {
     $view = new Template;
     echo $view->render('views/home.html');
 });
@@ -36,25 +35,67 @@ $f3->route('GET /', function($f3) {
 #-------------------------------------------------------------------------------
 #Route to personal-information.html
 $f3->route('GET|POST /personalInfo', function($f3) {
-    //$f3->set('page',"personal-information");
-    //$f3->set('title',"Personal Information");
-    $view = new Template;
-    echo $view->render('views/personal-information.html');
-});
-
-#-------------------------------------------------------------------------------
-#Route to profile.html
-$f3->route('POST /profile', function($f3) {
-    //$f3->set('page',"profile");
-    //$f3->set('title',"Profile");
-
+    #Initialize Errors
     $f3->set('errorFirstName', '');
     $f3->set('errorLastName', '');
     $f3->set('errorAge', '');
     $f3->set('errorPhone', '');
 
-    //array of states
-     $states=array("Alabama"=>"",
+
+    #if form is posted
+    if (!empty($_POST)) { // isset($_POST['submit'])
+        #validate
+        #if valid
+        if(validName($_POST['firstName'])
+            &&validName($_POST['lastName'])
+            &&validAge($_POST['age'])
+            &&validPhone($_POST['phoneNum'])) {
+
+            #assign session variables
+            $_SESSION['firstName']=$_POST['firstName'];
+            $_SESSION['lastName']=$_POST['lastName'];
+            $_SESSION['age']=$_POST['age'];
+            $_SESSION['phoneNum']=$_POST['phoneNum'];
+            $_SESSION['gender']=$_POST['gender'];
+
+            #reroute to profile.html
+            $f3->reroute('/profile');
+        }
+        else {
+            #Provide errors for invalid data
+            if(!validName($_POST['firstName'])) {$f3->set('errorFirstName',
+                "Please provide a first name.");}
+            if(!validName($_POST['lastName']))  {$f3->set('errorLastName',
+                "Please provide a last name.");}
+            if(!validAge($_POST['age']))        {$f3->set('errorAge',
+                "Please be 18 or older.");}
+            if(!validPhone($_POST['phoneNum'])) {$f3->set('errorPhone',
+                "Please provide a phone number in the following format : 2223334444");}
+        }
+
+        #make form sticky
+        if($_POST['gender']=="male"){$f3->set('male','checked');}
+        if($_POST['gender']=="female"){$f3->set('female','checked');}
+
+        $f3->set('firstName',$_POST['firstName']);
+        $f3->set('lastName',$_POST['lastName']);
+        $f3->set('age',$_POST['age']);
+        $f3->set('phoneNum',$_POST['phoneNum']);
+    }
+
+    #render personal-information.html
+    $view = new Template();
+    echo $view->render('views/personal-information.html');
+});
+
+#-------------------------------------------------------------------------------
+#Route to profile.html
+$f3->route('GET|POST /profile', function ($f3) {
+    #Initialize Errors
+    $f3->set('errorEmail', '');
+
+    #Initialize states array
+    $states=array("Alabama"=>"",
         "Alaska"=>"",
         "Arizona"=>"",
         "Arkansas"=>"",
@@ -104,52 +145,46 @@ $f3->route('POST /profile', function($f3) {
         "West Virginia"=>"",
         "Wisconsin"=>"",
         "Wyoming"=>"");
-     //if(!empty($_POST['state'])) {$states[$_POST['state']]='selected';}
-    if(!empty($_POST['state'])) {
-        $stateSelected=$_POST['state'];
-        $states[$stateSelected]= "selected";
-    }
-    $f3->set('states',$states);
-    $view = new Template;
 
-    #validate and assign data
-    if(validName($_POST['firstName'])
-        &&validName($_POST['lastName'])
-        &&validAge($_POST['age'])
-        &&validPhone($_POST['phoneNum'])) { #if all inputs are valid
-        $_SESSION['firstName']=$_POST['firstName'];
-        $_SESSION['lastName']=$_POST['lastName'];
-        $_SESSION['age']=$_POST['age'];
-        $_SESSION['phoneNum']=$_POST['phoneNum'];
-        $_SESSION['gender']=$_POST['gender'];
+    #If form is posted
+    if(!empty($_POST)) { // isset($_POST['submit'])
+        #validate
+        #if is valid
+        if (!($_POST['email']==='')) {
+            #assign session variables
+            $_SESSION['email']=$_POST['email'];
+            $_SESSION['state']=$_POST['state'];
+            $_SESSION['seeking']=$_POST['seeking'];
+            $_SESSION['biography']=$_POST['biography'];
 
-        echo $view->render('views/profile.html');
-    }#load next form
-    else { #any inputs are invalid
-        if(!validName($_POST['firstName'])) {$f3->set('errorFirstName',"Please provide a first name.");}
-        if(!validName($_POST['lastName']))  {$f3->set('errorLastName',"Please provide a last name.");}
-        if(!validAge($_POST['age']))        {$f3->set('errorAge',"Please be 18 or older.");}
-        if(!validPhone($_POST['phoneNum'])) {$f3->set('errorPhone',"Please provide a phone number 
-                                                in the following format : 2223334444");}
+            #reroute to interests
+            $f3->reroute('interests');
+         }
+        #provide errors for invalid data
+        $f3->set('errorEmail', 'Please provide an email address.');
+
         #make form sticky
-        if($_POST['gender']=="male"){$f3->set('male','checked');}
-        if($_POST['gender']=="female"){$f3->set('female','checked');}
+        if($_POST['seeking']=="borker"){$f3->set('borker','checked');}
+        if($_POST['seeking']=="moon-moon"){$f3->set('moonMoon','checked');}
 
-        $f3->set('firstName',$_POST['firstName']);
-        $f3->set('lastName',$_POST['lastName']);
-        $f3->set('age',$_POST['age']);
-        $f3->set('phoneNum',$_POST['phoneNum']);
+        $states[$_POST['state']]='selected';
 
+        $f3->set('biography',$_POST['biography']);
+    }
+    $f3->set('states', $states);
 
-        echo $view->render('views/personal-information.html');
-    }#reload page
+    #render profile.html
+    $view = new Template();
+    echo $view->render('views/profile.html');
 });
 
 #-------------------------------------------------------------------------------
 #Route to interests.html
-$f3->route('POST /interests', function($f3) {
-    //$f3->set('page',"interests");
-    //$f3->set('title',"Interests");
+$f3->route('GET|POST /interests', function ($f3) {
+    #Initialize Errors
+    $f3->set('errorInterests', '');
+
+    #Initialize indoor and outdoor arrays
     $f3->set('indoor', array("tv"=>"Watch T.V.", "couch"=>"Eating the couch",
         "barking"=>"Barking at Animals Outside", "board-games"=>"Board Games",
         "puzzles"=>"Puzzles", "reading"=>"Reading",
@@ -158,74 +193,59 @@ $f3->route('POST /interests', function($f3) {
     $f3->set('outdoor', array("hiking"=>"Hiking", "fetch"=>"Playing fetch",
         "swimming"=>"Swimming", "collectStick"=>"Collecting sticks",
         "walks"=>"WALKWALKWALK", "chaseCat"=>"Chasing Cats"));
-    $f3->set('errorEmail', ''); #initialize error as empty
-    $view = new Template;
 
-    if(!($_POST['email']==='')) { #if email is provided
-        $_SESSION['email']=$_POST['email'];
-        $_SESSION['state']=$_POST['state'];
-        $_SESSION['seeking']=$_POST['seeking'];
-        $_SESSION['biography']=$_POST['biography'];
+    #If form is posted
+    if(!empty($_POST)) { // isset($_POST['submit'])
+        #validate
+        #if is valid
+        if (validIndoor($_POST['inDoorInt'])&&
+            validOutdoor($_POST['outDoorInt'])) {
+            #assign session variables
+            $_SESSION['interests']= validIndoor($_POST['inDoorInt']);
+            $_SESSION['outerests']= validIndoor($_POST['outDoorInt']);
+            //$f3->set('interests', validIndoor($_POST['inDoorInt']));
+            //$f3->set('outerests', validIndoor($_POST['outDoorInt']));
 
-        echo $view->render('views/interests.html');
+            #if a gender is not chosen
+            if($_SESSION['gender']!='male'||$_SESSION['gender']!='female'){$f3->set('gender', 'Not Given');}
+            else{$f3->set('gender', $_SESSION['gender']);}
+
+            #reroute to interests
+            $f3->reroute('summary');
+        }
+        #provide errors for invalid data
+        $f3->set('errorInterests',
+            'What do you think you\'re you doing?');
+
+        #make form sticky
+        //$f3->set('selectedInt',$_POST['inDoorInt']);
+        //$f3->set('selectedOut',$_POST['outDoorInt']);
     }
-    else { #email was not provided
-        $f3->set('errorEmail', 'Please provide an email address.');
-
-        if($_POST['seeking']=="borker"){$f3->set('borker','checked');}
-        if($_POST['seeking']=="moon-moon"){$f3->set('moonMoon','checked');}
-
-        $f3->set('biography',$_POST['biography']);
-        echo $view->render('views/profile.html');
-    }
+    #render profile.html
+    $view = new Template();
+    echo $view->render('views/interests.html');
 });
 
 #-------------------------------------------------------------------------------
-#Create route to summary page
-$f3->route('POST /summary', function($f3) {
-    //$f3->set('page', 'summary');
-    //$f3->set('title',"Profile Summary");
-    $f3->set('errorInterests', '');
+#Route to summary.html
+$f3->route('GET|POST /summary', function ($f3) {
+    $f3->set('interests', $_SESSION['interests']);
+    $f3->set('outerests', $_SESSION['outerests']);
+    $f3->set('fName', $_SESSION['firstName']);
+    $f3->set('lName', $_SESSION['lastName']);
+    $f3->set('age', $_SESSION['age']);
+    $f3->set('gender', strtoupper($_SESSION['gender']));
+    $f3->set('phone', $_SESSION['phoneNum']);
+    $f3->set('email', $_SESSION['email']);
+    $f3->set('state', $_SESSION['state']);
+    $f3->set('seeking', $_SESSION['seeking']);
+    $f3->set('biography', $_SESSION['biography']);
+
+    #Render summary.html
     $view= new Template();
-
-    #interests are valid
-    if(validIndoor($_POST['inDoorInt'])&& validOutdoor($_POST['outDoorInt'])) {
-        $f3->set('interests', validIndoor($_POST['inDoorInt']));
-        $f3->set('outerests', validIndoor($_POST['outDoorInt']));
-
-        #if a gender is not chosen
-        if($_SESSION['gender']!='male'||$_SESSION['gender']!='female'){$f3->set('gender', 'Not Given');}
-        else{$f3->set('gender', $_SESSION['gender']);}
-
-        $f3->set('fName', $_SESSION['firstName']);
-        $f3->set('lName', $_SESSION['lastName']);
-        $f3->set('age', $_SESSION['age']);
-        $f3->set('phone', $_SESSION['phoneNum']);
-        $f3->set('email', $_SESSION['email']);
-        $f3->set('state', $_SESSION['state']);
-        $f3->set('seeking', $_SESSION['seeking']);
-
-        $f3->set('biography', $_SESSION['biography']);
-
-        echo $view->render('views/summary.html');
-    }#print summary
-    else { #interests are not valid
-        $f3->set('errorInterests', 'What are you doing?');
-        $f3->set('selectedInt',$_POST['inDoorInt']);
-        $f3->set('selectedOut',$_POST['outDoorInt']);
-        echo $view->render('views/interests.html');
-    }#reload form and STOP HACKING
+    echo $view->render('views/summary.html');
 });
 
 #-------------------------------------------------------------------------------
 #run fat free
 $f3->run();
-
-
-/*
- * test if a form has been submitted or not
- * if(isset($_POST['submit'])
- *
- * validate checkboxes. If valid, then go to next page
- *      else, stay on same page and display error.
- */
